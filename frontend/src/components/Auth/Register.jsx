00f157b9bcbc5b4aa63a919f78279fa14556480a
Rahu1kr <88312053+Rahu1kr@users.noreply.github.com>
2024-05-08 12:30:51 +0530

    placeholder change

--- frontend/src/components/Auth/Register.jsx
+++ frontend/src/components/Auth/Register.jsx
@@ -73,7 +73,7 @@ const Register = () => {
               <div>
                 <input
                   type="text"
-                  placeholder="Zeeshan"
+                  placeholder="Your name"
                   value={name}
                   onChange={(e) => setName(e.target.value)}
                 />
